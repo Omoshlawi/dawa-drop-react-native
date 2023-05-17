ef238859c011cc6b3f82a55eff81f744f6eca977
Laurent Ouma <lawiomosh3@gmail.com>
2023-05-17 13:43:54 +0300

    Splaying callout well

--- app/components/home/DeliveryRequest.js
+++ app/components/home/DeliveryRequest.js
@@ -1,9 +1,11 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
-import { Button, IconButton } from "react-native-paper";
+import { Avatar, Button, Card, IconButton, List } from "react-native-paper";
 import useLocation from "../../hooks/useLocation";
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Marker, Callout } from "react-native-maps";
 import colors from "../../utils/colors";
+import { screenWidth } from "../../utils/contants";
+import DeliveryRequestCallout from "./DeliveryRequestCallout";
 
 const DeliveryRequest = ({ request, setVisible }) => {
   const location = useLocation();
@@ -28,14 +30,26 @@ const DeliveryRequest = ({ request, setVisible }) => {
             }}
           >
             <Marker coordinate={location} title="Your current Location" />
-            {request.map(({ name, destination, address }, index) => (
-              <Marker coordinate={destination} title={name} key={index}>
-                <Image
-                  source={require("../../assets/hospitalmarker.png")}
-                  style={{ width: 60, height: 60 }}
-                />
-              </Marker>
-            ))}
+            {request.map((request_, index) => {
+              const { name, destination, address, delivery_mode, time_slot } =
+                request_;
+              return (
+                <Marker coordinate={destination} title={name} key={index}>
+                  <Image
+                    source={require("../../assets/hospitalmarker.png")}
+                    style={{ width: 60, height: 60 }}
+                  />
+                  <Callout
+                    style={styles.callOut}
+                    onPress={() => {
+                      console.log(request_);
+                    }}
+                  >
+                    <DeliveryRequestCallout request={request_} />
+                  </Callout>
+                </Marker>
+              );
+            })}
           </MapView>
         </View>
       )}
@@ -61,5 +75,8 @@ const styles = StyleSheet.create({
     zIndex: 1,
     top: 20,
     right: 20,
+  },
+  callOut: {
+    borderRadius: 10,
   },
 });
